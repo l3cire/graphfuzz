900ea7c0185cfc057f02cc0820ce55f09f41d5be
Vadim Zaripov <vadimzaripov@college.harvard.edu>
2025-12-08 17:26:54 -0500

    mst tester metamorphisms

--- Tester/MSTTester.py
+++ Tester/MSTTester.py
@@ -1,10 +1,10 @@
 import numpy as np
 from typing import Callable, Any
+import random
 
 import networkx as nx
 
-from Tester.BaseTester import BaseTester
-from Utils.FileUtils import save_discrepancies, save_discrepancy
+from Tester.BaseTester import BaseTester, TestMetamorphism
 from Utils.GraphConverter import GraphConverter
 
 
@@ -44,6 +44,67 @@ class MSTTesterAlgorithms:
         )
 
 
+class MSTTestMetamorphism(TestMetamorphism):
+    def mutate(
+        self, graph: nx.Graph, input: Any, result: int
+    ) -> tuple[nx.Graph, Any, Callable[[int], bool]]:
+        if len(graph.nodes) <= 1:
+            return graph, input, (lambda _: True)
+        graph_mutated, expected_result = self.compose_methods(graph, result)
+        checker = lambda res: (res == expected_result)
+        return graph_mutated, input, checker
+
+    def compose_methods(self, graph: nx.DiGraph, result: int, max_compositions=1):
+        n_compositions = random.randint(1, max_compositions)
+        all_methods = [
+            self.add_node_single_edge,
+        ]
+        if nx.is_connected(graph):
+            all_methods.extend(
+                [self.add_edge_large_weight, self.add_node_multiple_edges]
+            )
+        new_graph, new_result = graph, result
+        for _ in range(n_compositions):
+            method = random.choice(all_methods)
+            new_graph, new_result = method(new_graph, new_result)
+        return new_graph, new_result
+
+    def add_edge_large_weight(self, graph: nx.Graph, result: int):
+        weight = result + 1
+        nodes = list(graph.nodes)
+        for _ in range(1000):
+            u, v = random.sample(nodes, 2)
+            if graph.has_edge(u, v):
+                continue
+            new_graph = graph.copy()
+            new_graph.add_edge(u, v, weight=weight)
+            return new_graph, result
+        return graph, result
+
+    def add_node_single_edge(self, graph: nx.Graph, result: int):
+        new_node = max(list(graph.nodes)) + 1
+        weight = random.randint(1, 20)
+        some_node = random.choice(list(graph.nodes))
+        new_graph = graph.copy()
+        new_graph.add_node(new_node)
+        new_graph.add_edge(new_node, some_node, weight=weight)
+        return new_graph, result + weight
+
+    def add_node_multiple_edges(self, graph: nx.Graph, result: int, max_n_edges=10):
+        new_node = max(list(graph.nodes)) + 1
+        n_edges = random.randint(1, min(len(list(graph.nodes)), max_n_edges))
+
+        weights = [random.randint(result, result + 100) for _ in range(n_edges)]
+        nodes = random.sample(list(graph.nodes), n_edges)
+
+        new_graph = graph.copy()
+        new_graph.add_node(new_node)
+        for weight, old_node in zip(weights, nodes):
+            new_graph.add_edge(new_node, old_node, weight=weight)
+
+        return new_graph, result + min(weights)
+
+
 class MSTTester(BaseTester):
 
     def __init__(
@@ -51,11 +112,14 @@ class MSTTester(BaseTester):
     ):
         super().__init__(corpus_path, discrepancy_filename, *args, **kwargs)
         self.algorithms: dict[str, Callable[[nx.DiGraph], Any]] = {
-            "kuskal": MSTTesterAlgorithms.kruskal,
+            "kruskal": MSTTesterAlgorithms.kruskal,
             "prim": MSTTesterAlgorithms.prim,
             "boruvka": MSTTesterAlgorithms.boruvka,
             "igraph": MSTTesterAlgorithms.igraph,
         }
+
+    def get_test_metamorphism(self):
+        return MSTTestMetamorphism()
 
     def preprocess_weights(self, graph: nx.DiGraph):
         if not nx.is_weighted(graph):
