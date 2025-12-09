f7fa0d8f7cbea4cce326cde7b69e36a6d6818c1c
Vadim Zaripov <vadimzaripov@college.harvard.edu>
2025-12-09 15:22:06 -0500

    bcc metamorphism

--- Tester/BCCTester.py
+++ Tester/BCCTester.py
@@ -1,9 +1,9 @@
 from typing import Callable, Any
+import random
 
 import networkx as nx
 
-from Tester.BaseTester import BaseTester
-from Utils.FileUtils import save_discrepancy
+from Tester.BaseTester import BaseTester, TestMetamorphism
 from Utils.GraphConverter import GraphConverter
 
 
@@ -22,6 +22,30 @@ class BCCTesterAlgorithms:
         )
 
 
+class BCCTestMetamorphism(TestMetamorphism):
+    def mutate(
+        self, graph: nx.Graph, input: Any, result: set[frozenset[int]]
+    ) -> tuple[nx.Graph, Any, Callable[[set[frozenset[int]]], bool]]:
+        if len(result) == 0:
+            return graph, input, lambda _: True
+        new_graph = graph.copy()
+        new_node = max(list(graph.nodes)) + 1
+        new_graph.add_node(new_node)
+
+        comp = random.choice(list(result))
+        n_neighbours = random.randint(1, len(list(comp)))
+        neighbours = random.sample(list(comp), n_neighbours)
+        new_graph.add_edges_from([(new_node, node) for node in neighbours])
+
+        def check(components: set[frozenset[int]]):
+            if n_neighbours == 1:
+                return comp in components
+            else:
+                return comp.union({new_node}) in components
+
+        return new_graph, input, check
+
+
 class BCCTester(BaseTester):
 
     def __init__(
@@ -32,3 +56,6 @@ class BCCTester(BaseTester):
             "networkx": BCCTesterAlgorithms.networkx,
             "igraph": BCCTesterAlgorithms.igraph,
         }
+
+    def get_test_metamorphism(self):
+        return BCCTestMetamorphism()
